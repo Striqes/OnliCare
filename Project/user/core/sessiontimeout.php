7fd4406fd5af523c6aa6bd87bab922d83ee2ad0d
Lucky Emmanuelle Asuncion <darkblade902@gmail.com>
2024-05-12 14:35:21 +0800

    Added session destroy

--- Project/user/core/sessiontimeout.php
+++ Project/user/core/sessiontimeout.php
@@ -22,12 +22,38 @@ if(isset($_SESSION['timeout'])) {
 $_SESSION['timeout'] = time();
 
 // Check if user is logged in
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) && !endsWith($_SERVER['REQUEST_URI'], 'index.php')) {
   // Redirect to login page
   header('Location: ../login.php');
   exit;
 }
 
-echo "User ID: " . $_SESSION['user_id'];
+// Check if user is logged in
+if (isset($_SESSION['user_id'])) {
+  // Display the button only if user is logged in
+  echo "User ID: " . $_SESSION['user_id'];
+  echo '<form method="post">';
+  echo '<button type="submit" name="logout">Destroy Session</button>';
+  echo '</form>';
+}
+
+function endsWith($haystack, $needle) {
+  $length = strlen($needle);
+  if ($length == 0) {
+      return true;
+  }
+  return (substr($haystack, -$length) === $needle);
+}
+
+if (isset($_POST['logout'])) {
+  // Unset all session variables
+  $_SESSION = array();
+
+  // Destroy the session
+  session_destroy();
+  $absolutePath = $root . "/onlicare/Project/index.php";
+  header("Location: $absolutePath");
+  
+}
 
 ?>
