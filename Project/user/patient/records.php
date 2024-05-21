5f463d8f93f00fc980db7e3159ead678acee2ebd
Lucky Emmanuelle Asuncion <darkblade902@gmail.com>
2024-05-21 14:07:10 +0800

    Fixed header and removed extras | Onlicare Logo now redirects to index

--- Project/user/patient/records.php
+++ Project/user/patient/records.php
@@ -88,7 +88,7 @@ if (!$PatientRow) {
     <header>
         <nav class="bg-white border-b border-gray-200 dark:bg-green-900">
             <div class="max-w-screen-xl mx-auto px-4 py-6 flex items-center justify-between">
-                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="<?php echo $indexPath;?>" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="<?php echo "$url_root" . "assets/onlicarelogo.svg";?>" class="h-10" alt="Logo" />
                     <span class="text-2xl font-semibold whitespace-nowrap dark:text-yellow-400">OnliCare</span>
                 </a>
@@ -161,7 +161,7 @@ if (!$PatientRow) {
     <!-- FOOTER -->
     <footer class="bg-white shadow-md dark:bg-green-900">
         <div class="max-w-screen-xl mx-auto p-4 md:p-8 flex flex-col md:flex-row items-center justify-between">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse mb-4 md:mb-0">
+            <a href="<?php echo $indexPath;?>" class="flex items-center space-x-3 rtl:space-x-reverse mb-4 md:mb-0">
                 <img src="..\assets\onlicarelogo.svg" class="h-8" alt="Logo">
                 <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">OnliCare</span>
             </a>
